bf88dde66cb2d4e42e42dd5d1b0fe8aaf5a0ea71
Alfred Ochieng <techyobura@gmail.com>
2024-08-13 15:57:00 +0300

    sign-up pink color adjustment for a test

--- src/auth/SignUp.jsx
+++ src/auth/SignUp.jsx
@@ -1,29 +1,38 @@
-import React, { useState } from 'react';
-import { register } from '../utils/auth';
-import Navbar from '../components/Navbar';
-import { Toast } from '../plugins/Toast';
-import { useNavigate } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import React, { useState } from "react";
+import { register } from "../utils/auth";
+import Navbar from "../components/Navbar";
+import { Toast } from "../plugins/Toast";
+import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 function SignUp() {
   const navigate = useNavigate();
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [password2, setPassword2] = useState('');
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
 
   const handleSubmit = async () => {
     if (password !== password2) {
-      Toast("error", "Passwords do not match", "Please ensure that the passwords match.");
+      Toast(
+        "error",
+        "Passwords do not match",
+        "Please ensure that the passwords match."
+      );
       return;
     }
 
     console.log(fullName, email, password, password2);
-    const { data, error } = await register(fullName, email, password, password2);
+    const { data, error } = await register(
+      fullName,
+      email,
+      password,
+      password2
+    );
     if (data) {
-      navigate('/login');
+      navigate("/login");
     } else {
-      Toast("error", error, 'Try again later!');
+      Toast("error", error, "Try again later!");
     }
   };
 
@@ -31,65 +40,83 @@ function SignUp() {
     <>
       <Helmet>
         <title>Sign Up - SERA Innovations</title>
-        <meta name="description" content="Create an account with SERA Innovations to get started on managing your projects efficiently. Sign up now to access all our digital solutions." />
-        <meta name="keywords" content="Serainnovations, Sera Innovations, sign up, register, account creation, project management, digital solutions" />
+        <meta
+          name="description"
+          content="Create an account with SERA Innovations to get started on managing your projects efficiently. Sign up now to access all our digital solutions."
+        />
+        <meta
+          name="keywords"
+          content="Serainnovations, Sera Innovations, sign up, register, account creation, project management, digital solutions"
+        />
         <meta property="og:title" content="Sign Up - SERA Innovations" />
-        <meta property="og:description" content="Create an account with SERA Innovations to get started on managing your projects efficiently." />
-        <meta property="og:image" content="https://serainnovations.pro/path-to-your-image.jpg" />
+        <meta
+          property="og:description"
+          content="Create an account with SERA Innovations to get started on managing your projects efficiently."
+        />
+        <meta
+          property="og:image"
+          content="https://serainnovations.pro/path-to-your-image.jpg"
+        />
         <meta property="og:url" content="https://serainnovations.pro/signup" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Sign Up - SERA Innovations" />
-        <meta name="twitter:description" content="Create an account with SERA Innovations to get started on managing your projects efficiently." />
-        <meta name="twitter:image" content="https://serainnovations.pro/path-to-your-image.jpg" />
+        <meta
+          name="twitter:description"
+          content="Create an account with SERA Innovations to get started on managing your projects efficiently."
+        />
+        <meta
+          name="twitter:image"
+          content="https://serainnovations.pro/path-to-your-image.jpg"
+        />
       </Helmet>
       <Navbar />
       <main className="mt-10 pt-5">
-        <section className='flex md:flex-row flex-col gap-2'>
+        <section className="flex md:flex-row flex-col gap-2">
           <div className="login p-20 bg-orange-500 shadow-lg">
             <header>
-              <h1 className='md:text-5xl text-3xl mx-5 font-bold'>Sign up</h1>
+              <h1 className="md:text-5xl text-3xl mx-5 font-bold">Sign up</h1>
             </header>
-            <form className='flex flex-col m-5'>
+            <form className="flex flex-col m-5">
               <label htmlFor="fullName">Full Name</label>
-              <input 
-                onChange={(e) => setFullName(e.target.value)} 
-                placeholder='Name' 
-                className='my-2 blockS h-9 border border-gray-200 rounded-lg p-3 hover:outline outline-blue-500 outline-1' 
-                type="text" 
-                id="fullName" 
-                required 
+              <input
+                onChange={(e) => setFullName(e.target.value)}
+                placeholder="Name"
+                className="my-2 blockS h-9 border border-gray-200 rounded-lg p-3 hover:outline outline-blue-500 outline-1"
+                type="text"
+                id="fullName"
+                required
               />
               <label htmlFor="email">Email</label>
-              <input 
-                onChange={(e) => setEmail(e.target.value)} 
-                placeholder='Enter your email id' 
-                className='my-2 h-9 rounded-lg border border-gray-200 p-3 hover:outline outline-blue-500 outline-1' 
-                type="email" 
-                id="email" 
-                required 
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter your email id"
+                className="my-2 h-9 rounded-lg border border-gray-200 p-3 hover:outline outline-blue-500 outline-1"
+                type="email"
+                id="email"
+                required
               />
               <label htmlFor="password">Password</label>
-              <input 
-                onChange={(e) => setPassword(e.target.value)} 
-                placeholder='Create a Password' 
-                className='my-2 h-9 rounded-lg border border-gray-200 p-3 hover:outline outline-blue-500 outline-1' 
-                type="password" 
-                id="password" 
-                required 
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Create a Password"
+                className="my-2 h-9 rounded-lg border border-gray-200 p-3 hover:outline outline-blue-500 outline-1"
+                type="password"
+                id="password"
+                required
               />
               <label htmlFor="password2">Confirm Password</label>
-              <input 
-                onChange={(e) => setPassword2(e.target.value)} 
-                placeholder='Re-type your Password' 
-                className='my-2 h-9 rounded-lg border border-gray-200 p-3 hover:outline outline-blue-500 outline-1' 
-                type="password" 
-                id="password2" 
-                required 
+              <input
+                onChange={(e) => setPassword2(e.target.value)}
+                placeholder="Re-type your Password"
+                className="my-2 h-9 rounded-lg border border-gray-200 p-3 hover:outline outline-blue-500 outline-1"
+                type="password"
+                id="password2"
+                required
               />
-              <button 
-                onClick={handleSubmit} 
-                className='rounded-lg hover:bg-black hover:text-orange-500 bg-orange-200 h-fit my-1 p-3 text-black font-bold'
+              <button
+                onClick={handleSubmit}
+                className="rounded-lg hover:bg-pink-300 hover:text-orange-500 bg-orange-200 h-fit my-1 p-3 text-black font-bold"
                 type="button"
               >
                 Sign up
@@ -97,7 +124,11 @@ function SignUp() {
             </form>
           </div>
           <aside className="img hidden md:block ml-56 md:ml-20">
-            <img className='h-[500px] w-[500px] flex-wrap' src="/signup-now.png" alt="Sign up" />
+            <img
+              className="h-[500px] w-[500px] flex-wrap"
+              src="/signup-now.png"
+              alt="Sign up"
+            />
           </aside>
         </section>
       </main>
